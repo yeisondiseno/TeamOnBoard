72a8b616572ef32e2ded1dbf65d42d07e4061d9f
Yeison Montoya <yeisondiseno@gmail.com>
2020-12-03 16:19:47 -0500

    Y.Montoya: Ubicación básica

---
+++ src/pages/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="head">
+        <div className="logo">
+          Logo
+        </div>
+        <div className="menu">
+           menu 
+        </div>
+        <div className="user">
+          user
+        </div>
+      </div>
+      <div className="left">
+        <div className="calendar">
+          calendar
+        </div>
+        <div className="schedule">
+          schedule
+        </div>
+      </div>
+      <div className="content">
+        content
+      </div>
+    </div>
+  );
+}
+
+export default App;
